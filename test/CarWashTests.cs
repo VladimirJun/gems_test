4941583bc1c5bce83419322f1c42879a070f0fcc
VladimirJun <Vovaboy3000@mail.ru>
2025-09-06 17:00:19 +0600

    added test runner

---
+++ test/CarWashTests.cs
@@ -0,0 +1,95 @@
+﻿using CarWash.models;
+
+namespace CarWash.test
+{
+    public class CarWashTests
+    {
+        public void RevenueCalculation_WorksCorrectly()
+        {
+            var wash = new CarWash.service.CarWash(2);
+            wash.AddCar(new Car(CarType.Passenger)); // 5 мин
+            wash.AddCar(new Car(CarType.Suv));       // 8 мин
+            wash.AddCar(new Car(CarType.Van));       // очередь
+
+            wash.Simulate(10);
+
+            decimal expectedRevenue = (5 + 8 + 5) * 30; // 540
+
+            if (wash.GetRevenue() != expectedRevenue)
+                throw new Exception($"RevenueCalculation_WorksCorrectly failed: expected {expectedRevenue}, got {wash.GetRevenue()}");
+        }
+
+        public void QueueProcessing_WorksCorrectly()
+        {
+            var wash = new CarWash.service.CarWash(1);
+            wash.AddCar(new Car(CarType.Passenger)); 
+            wash.AddCar(new Car(CarType.Suv));       
+
+            wash.Simulate(5);
+            wash.Simulate(3);
+
+            if (wash.GetRevenue() != 8 * 30)
+                throw new Exception($"QueueProcessing_WorksCorrectly failed: expected {8*30}, got {wash.GetRevenue()}");
+        }
+
+        public void AddingCars_Fills_Queue_When_Stations_Full()
+        {
+            var wash = new CarWash.service.CarWash(2);
+            var car1 = new Car(CarType.Passenger);
+            var car2 = new Car(CarType.Suv);
+            var car3 = new Car(CarType.Van);
+
+            wash.AddCar(car1);
+            wash.AddCar(car2);
+            wash.AddCar(car3);
+
+            wash.Simulate(1);
+
+            if (wash.GetRevenue() / 30 != 2)
+                throw new Exception($"AddingCars_Fills_Queue_When_Stations_Full failed: expected 2, got {wash.GetRevenue()/30}");
+
+            if (wash.QueueLen() != 1)
+                throw new Exception($"AddingCars_Fills_Queue_When_Stations_Full failed: expected queue length 1, got {wash.QueueLen()}");
+        }
+
+        public void ZeroCars_NoRevenue()
+        {
+            var wash = new CarWash.service.CarWash(3);
+            var len = wash.QueueLen();
+            wash.Simulate(10);
+
+            if (wash.GetRevenue() != 0)
+                throw new Exception($"ZeroCars_NoRevenue failed: expected 0, got {wash.GetRevenue()}");
+
+            if (len != 0)
+                throw new Exception($"ZeroCars_NoRevenue failed: expected queue length 0, got {len}");
+        }
+
+        public void NegativeStations_ThrowsException()
+        {
+            try
+            {
+                var wash = new CarWash.service.CarWash(-1);
+                throw new Exception("NegativeStations_ThrowsException failed: exception not thrown");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // всё ок
+            }
+        }
+
+        public void CarWithUnknownType_ThrowsException()
+        {
+            try
+            {
+                var badCar = new Car((CarType)999);
+                throw new Exception("CarWithUnknownType_ThrowsException failed: exception not thrown");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // всё ок
+            }
+        }
+    }
+    
+}
---
+++ test/TestRunner.cs
@@ -0,0 +1,30 @@
+﻿namespace CarWash.test
+{
+    public static class TestRunner
+    {
+        public static void RunAll()
+        {
+            var tests = new CarWashTests();
+
+            RunTest(() => tests.RevenueCalculation_WorksCorrectly(), nameof(tests.RevenueCalculation_WorksCorrectly));
+            RunTest(() => tests.QueueProcessing_WorksCorrectly(), nameof(tests.QueueProcessing_WorksCorrectly));
+            RunTest(() => tests.AddingCars_Fills_Queue_When_Stations_Full(), nameof(tests.AddingCars_Fills_Queue_When_Stations_Full));
+            RunTest(() => tests.ZeroCars_NoRevenue(), nameof(tests.ZeroCars_NoRevenue));
+            RunTest(() => tests.NegativeStations_ThrowsException(), nameof(tests.NegativeStations_ThrowsException));
+            RunTest(() => tests.CarWithUnknownType_ThrowsException(), nameof(tests.CarWithUnknownType_ThrowsException));
+        }
+
+        private static void RunTest(Action testMethod, string testName)
+        {
+            try
+            {
+                testMethod();
+                Console.WriteLine($"{testName}: OK");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{testName}: FAIL -> {ex.Message}");
+            }
+        }
+    }
+}
