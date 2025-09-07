df425102e00a4e74aa72979453642386b81d847c
VladimirJun <Vovaboy3000@mail.ru>
2025-09-08 01:18:29 +0600

    edit runAll() method

--- test/TestRunner.cs
+++ test/TestRunner.cs
@@ -6,12 +6,13 @@
         {
             var tests = new CarWashTests();
 
-            RunTest(() => tests.RevenueCalculation_WorksCorrectly(), nameof(tests.RevenueCalculation_WorksCorrectly));
-            RunTest(() => tests.QueueProcessing_WorksCorrectly(), nameof(tests.QueueProcessing_WorksCorrectly));
-            RunTest(() => tests.AddingCars_Fills_Queue_When_Stations_Full(), nameof(tests.AddingCars_Fills_Queue_When_Stations_Full));
-            RunTest(() => tests.ZeroCars_NoRevenue(), nameof(tests.ZeroCars_NoRevenue));
-            RunTest(() => tests.NegativeStations_ThrowsException(), nameof(tests.NegativeStations_ThrowsException));
-            RunTest(() => tests.CarWithUnknownType_ThrowsException(), nameof(tests.CarWithUnknownType_ThrowsException));
+            RunTest(tests.RevenueCalculation_WorksCorrectly, nameof(tests.RevenueCalculation_WorksCorrectly));
+            RunTest(tests.QueueProcessing_WorksCorrectly, nameof(tests.QueueProcessing_WorksCorrectly));
+            RunTest(tests.AddingCars_Fills_Queue_When_Stations_Full, nameof(tests.AddingCars_Fills_Queue_When_Stations_Full));
+            RunTest(tests.ZeroCars_NoRevenue, nameof(tests.ZeroCars_NoRevenue));
+            RunTest(tests.NegativeStations_ThrowsException, nameof(tests.NegativeStations_ThrowsException));
+            RunTest(tests.CarWithUnknownType_ThrowsException, nameof(tests.CarWithUnknownType_ThrowsException));
+
         }
 
         private static void RunTest(Action testMethod, string testName)
